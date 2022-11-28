1ab411a24a6a1615e1f6c2a895f0f864d1b08cce
Michael-Mehta <supermichael2001@gmail.com>
2022-11-28 09:17:22 -0700

    Added four dropdown menus

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,12 @@
+class FlightsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def index
+    end
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = []
+
+airports[0] = Airport.create(code: "PHX", name:"Sky Harbor", location:"Phoenix")
+airports[1] = Airport.create(code: "LA", name:"Long Beach Airport", location:"Los Angeles")
+airports[2] = Airport.create(code: "MIA", name:"Miami Airlines", location: "Miami")
+
+
+
+flights = Flight.create([{departure_airport_id: airports.first.id, arrival_airport_id: airports.last.id, duration: 400, scheduled_on: "2022-12-25 07:00:00"},
+{departure_airport_id: airports.first.id, arrival_airport_id: airports.last.id, duration: 430, scheduled_on: "2022-12-26 08:00:00"},
+{departure_airport_id: airports.first.id, arrival_airport_id: airports.last.id, duration: 405, scheduled_on: "2022-12-27 09:00:00"} ])
