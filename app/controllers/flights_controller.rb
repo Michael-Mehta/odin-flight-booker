342dbcc8567e2d51ec720438b15d96fea2aab470
Michael-Mehta <supermichael2001@gmail.com>
2022-11-28 10:18:22 -0700

    Pulls matching flights that match search criteria

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,21 @@
 class FlightsController < ApplicationController
 
     def new
+        @flight = Flight.new
+        selected_date = Date.civil(params[:year].to_i,params[:month].to_i,params[:day].to_i)
+        @available_flights = Flight.where(departure_airport_id: params[:departure_airport_id], arrival_airport_id: params
+        [:arrival_airport_id], scheduled_on: selected_date.all_day)
     end
 
     def create
+        @flight = Flight.new(flight_params)
+        @flight.save
+        redirect_to root_path
     end
 
     def index
+        @flights = Flight.all
+
     end
-    
+
 end
