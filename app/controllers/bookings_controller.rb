d0c426eb14b7028ddf8b0001c65953200fdb83c0
Michael-Mehta <supermichael2001@gmail.com>
2022-11-29 14:05:23 -0700

    Implement accepts_nested_attributes in Booking model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,25 @@
 class BookingsController < ApplicationController
 
+include BookingsHelper
+
     def new
         @booking = Booking.new
         @selected_flight = Flight.find(params[:flight])
         @passengers_count = params[:passengers].to_i
         @passengers_count.times{@booking.passengers.build}
+    end
+
+    def create
+
+        @booking = Booking.new(booking_params)
+
+        if @booking.save
+
+            flash[:success] = 'Booking saved!'
+            redirect_to booking_path(@booking)
+        else
+            flash[:alert] = "An error has occured!"
+            render "new"
+        end
     end
 end
--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,6 @@
 module BookingsHelper
+
+    def booking_params
+        params.require(:booking).permit(:flight_id, passenger_attributes: [:name, :email])
+    end
 end
