89364846d9749724dbd2caff4a70877a085be1ad
Michael-Mehta <supermichael2001@gmail.com>
2022-11-29 11:35:46 -0700

    Set up Bookings new action and passenger view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+
+    def new
+        @booking = Booking.new
+        @selected_flight = Flight.find(params[:flight])
+        @passengers_count = params[:passengers].to_i
+        @passengers_count.times{@booking.passengers.build}
+    end
 end
