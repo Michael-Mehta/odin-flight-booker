7a4eb2eb1701dec50923e6f976c0120adfecc3e0
Michael-Mehta <supermichael2001@gmail.com>
2022-12-06 19:57:00 -0700

    Added booking_confirmation method to passnger_mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,11 @@
 class PassengerMailer < ApplicationMailer
+
+    def booking_confirmation
+        @passenger = params[:passenger]
+        @booking = params[:booking]
+        @flight = params[:flight]
+
+        mail(to: @passenger.email, subject: "Confirmation Email")
+    end
+
 end
